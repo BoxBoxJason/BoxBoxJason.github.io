bbe27eb6018237c2d0db4359e31631bbe8c960c5
BoxBoxJason <loickgoupil@yahoo.fr>
2024-05-24 14:05:44 +0100

    Update header & add mobile view support

---
+++ assets/js/header.js
@@ -0,0 +1,34 @@
+"use strict";
+var menu_visible = false;
+setMenuButtonAction();
+function setMenuButtonAction() {
+    let menu_button = document.getElementById('menu-button');
+    let menu_image = document.getElementById('menu-image');
+    let header = document.querySelector('header');
+    let main = document.querySelector('main');
+    let footer = document.querySelector('footer');
+    console.log(header);
+    console.log(main);
+    console.log(footer);
+    if (menu_button && header && main && footer && menu_image) {
+        menu_button.addEventListener('click', function () {
+            if (menu_visible) {
+                header.style.display = 'none';
+                main.style.display = 'block';
+                footer.style.display = 'flex';
+                menu_image.src = '../assets/images/triple-bar.webp';
+                menu_button.style.position = 'fixed';
+            }
+            else {
+                header.style.display = 'block';
+                main.style.display = 'none';
+                footer.style.display = 'none';
+                menu_image.src = '../assets/images/x-close.webp';
+                menu_button.style.position = 'absolute';
+            }
+            menu_visible = !menu_visible;
+        });
+    } else {
+        console.error('Error, at least one of the components for the menu action was not found !');
+    }
+}
---
+++ assets/ts/header.ts
@@ -0,0 +1,31 @@
+var menu_visible = false;
+
+setMenuButtonAction()
+
+function setMenuButtonAction() {
+    let menu_button = document.getElementById('menu-button') as HTMLButtonElement;
+    let menu_image = document.getElementById('menu-image') as HTMLImageElement;
+    let header = document.querySelector<HTMLElement>('header');
+    let main = document.querySelector<HTMLElement>('main');
+    let footer = document.querySelector<HTMLElement>('footer');
+    if(menu_button && header && main && footer && menu_image) {
+        menu_button.addEventListener('click',function() {
+            if(menu_visible) {
+                header.style.display = 'none';
+                main.style.display = 'block';
+                footer.style.display = 'flex';
+                menu_image.src = '../assets/images/triple-bar.webp';
+                menu_button.style.position = 'fixed';
+            } else {
+                header.style.display = 'block';
+                main.style.display = 'none';
+                footer.style.display = 'none';
+                menu_image.src = '../assets/images/x-close.webp';
+                menu_button.style.position = 'absolute';
+            }
+            menu_visible = !menu_visible;
+        });
+    } else {
+        console.error('Error, at least one of the components for the menu action was not found !');
+    }
+}
